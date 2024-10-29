039e50f3a6173904cb28eb522a641e86bed7f670
aibout lakhdar <laibout@intelgx.com>
2024-10-29 17:01:44 +0100

    update

--- src/components/onboarding/circularPrgress.jsx
+++ src/components/onboarding/circularPrgress.jsx
@@ -1,37 +1,30 @@
 import React from 'react';
-import { View, StyleSheet,Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
-import { Circle } from 'react-native-svg';
 
-
-const CircularProgressbar = ({ progress=0}) => {
-
+const CircularProgressbar = ({ progress = 0 }) => {
   return (
     <View style={styles.container}>
-
-
-
-<AnimatedCircularProgress
-  childrenContainerStyle={styles.test}
-  rotation={- 360}
-  lineCap="round"
-  delay={500}
-  duration={2000}
-  size={200}
-  width={15}
-  fill={progress}
-  tintColor="#bb2124"
-  backgroundColor="#3d5875"
-  tintColorSecondary="#22bb33"
-  >
-  {
-    (fill) => (
-      <Text>
-        { progress }/100
-      </Text>
-    )
-  }
-</AnimatedCircularProgress>
+      <AnimatedCircularProgress
+        childrenContainerStyle={styles.test}
+        rotation={-360}
+        lineCap="round"
+        delay={500}
+        duration={2000}
+        size={200}
+        width={15}
+        fill={progress}
+        tintColor="#FF474D"
+        tintColorSecondary="#5ce600"
+        backgroundColor="#3d5875"
+      >
+        {(fill) => (
+           <Text style={styles.progressText}>
+           {progress}
+           <Text style={styles.totalText}>/100</Text>
+         </Text>
+        )}
+      </AnimatedCircularProgress>
     </View>
   );
 };
@@ -42,8 +35,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginVertical: 20,
   },
-
-  
+  progressText: {
+    fontSize: 50,
+    color: 'white',
+    fontFamily:"Tajawal-Bold"
+  },
+  totalText:{
+    fontSize: 20,
+    color: 'white',
+    fontFamily:"Tajawal-Bold"
+  }
 });
 
 export default CircularProgressbar;
